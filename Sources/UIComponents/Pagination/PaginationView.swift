0d873515ef9b9c7efff9dbca532d5888d69be297
Nikita Kazantsev <kaznik2013@gmail.com>
2022-01-16 17:19:48 +0300

    pagingview missing code restored

--- Sources/UIComponents/Pagination/PaginationView.swift
+++ Sources/UIComponents/Pagination/PaginationView.swift
@@ -4,44 +4,44 @@ public struct PaginationView<Content: View>: View {
 
     // MARK: - Properties
 
-	@Binding private var index: Int
-	@State private var offset = CGFloat.zero
-	@State private var dragging = false
+    @Binding private var index: Int
+    @State private var offset = CGFloat.zero
+    @State private var dragging = false
 
     private let maxIndex: Int
     private let content: () -> Content
 
-	public var body: some View {
-		ZStack(alignment: .bottomTrailing) {
-			GeometryReader { geometry in
-				ScrollView(.horizontal, showsIndicators: false) {
-					HStack(spacing: 0) {
-						self.content()
-							.frame(width: geometry.size.width, height: geometry.size.height)
-							.clipped()
-					}
-				}
-				.content.offset(x: self.offset(in: geometry), y: 0)
-				.frame(width: geometry.size.width, alignment: .leading)
-				.gesture(
-					DragGesture().onChanged { value in
-						self.dragging = true
-						self.offset = -CGFloat(self.index) * geometry.size.width + value.translation.width
-					}
-					.onEnded { value in
-						let predictedEndOffset = -CGFloat(self.index) * geometry.size.width + value.predictedEndTranslation.width
-						let predictedIndex = Int(round(predictedEndOffset / -geometry.size.width))
-						self.index = self.clampedIndex(from: predictedIndex)
-						withAnimation(.easeOut) {
-							self.dragging = false
-						}
-					}
-				)
-			}
-			.clipped()
+    public var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            GeometryReader { geometry in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        self.content()
+                            .frame(width: geometry.size.width, height: geometry.size.height)
+                            .clipped()
+                    }
+                }
+                .content.offset(x: self.offset(in: geometry), y: 0)
+                .frame(width: geometry.size.width, alignment: .leading)
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.dragging = true
+                        self.offset = -CGFloat(self.index) * geometry.size.width + value.translation.width
+                    }
+                        .onEnded { value in
+                            let predictedEndOffset = -CGFloat(self.index) * geometry.size.width + value.predictedEndTranslation.width
+                            let predictedIndex = Int(round(predictedEndOffset / -geometry.size.width))
+                            self.index = self.clampedIndex(from: predictedIndex)
+                            withAnimation(.easeOut) {
+                                self.dragging = false
+                            }
+                        }
+                )
+            }
+            .clipped()
             PageControlView(index: $index, maxIndex: maxIndex)
-		}
-	}
+        }
+    }
 
     // MARK: - Init
 
@@ -53,41 +53,46 @@ public struct PaginationView<Content: View>: View {
 
     // MARK: - Private methods
 
-	private func offset(in geometry: GeometryProxy) -> CGFloat {
-		if self.dragging {
-			return max(min(self.offset, 0), -CGFloat(self.maxIndex) * geometry.size.width)
-		} else {
-			return -CGFloat(self.index) * geometry.size.width
-		}
-	}
+    private func offset(in geometry: GeometryProxy) -> CGFloat {
+        if self.dragging {
+            return max(min(self.offset, 0), -CGFloat(self.maxIndex) * geometry.size.width)
+        } else {
+            return -CGFloat(self.index) * geometry.size.width
+        }
+    }
 
-	private func clampedIndex(from predictedIndex: Int) -> Int {
-		let newIndex = min(max(predictedIndex, index - 1), index + 1)
-		guard newIndex >= 0 else {
-			return 0
-		}
-		guard newIndex <= self.maxIndex else {
-			return self.maxIndex
-		}
-		return newIndex
-	}
+    private func clampedIndex(from predictedIndex: Int) -> Int {
+        let newIndex = min(max(predictedIndex, index - 1), index + 1)
+        guard newIndex >= 0 else {
+            return 0
+        }
+        guard newIndex <= self.maxIndex else {
+            return self.maxIndex
+        }
+        return newIndex
+    }
 }
 
 // MARK: - PageControlView
 
 struct PageControlView: View {
-	@Binding var index: Int
-	let maxIndex: Int
+    @Binding var index: Int
+    let maxIndex: Int
 
-	var body: some View {
-		HStack(spacing: 8) {
-			ForEach(0...maxIndex, id: \.self) { index in
-				Circle()
-					.fill(index == self.index ? Color.white : Color.gray)
-					.frame(width: 8, height: 8)
-			}
-		}
-		.padding(16)
-	}
+    var body: some View {
+        HStack(spacing: 8) {
+            ForEach(0 ... maxIndex, id: \.self) { index in
+                Circle()
+                    .fill(index == self.index ? Color(.lightGray) : Color.white)
+                    .frame(width: 8, height: 8)
+            }
+        }
+        .padding(.vertical, 3)
+        .padding(.horizontal, 4)
+        .background(Color.black.opacity(0.1))
+        .cornerRadius(4)
+        .padding(.trailing, 16)
+        .padding(.bottom, 8)
+    }
 }
 
